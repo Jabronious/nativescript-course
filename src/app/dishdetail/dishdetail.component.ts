b8d4d1a773f9d8d5bf46d55b5bebcba5054df2a5
Jarrod Merryman <jarrodmerryman@gmail.com>
2019-01-27 15:58:42 -0500

    images for icons/splash screen

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -11,6 +11,12 @@ import * as Toast from "nativescript-toast";
 import { action } from "ui/dialogs";
 import { ModalDialogService, ModalDialogOptions } from "nativescript-angular/modal-dialog";
 import { CommentComponent } from "../comment/comment.component";
+import { Page } from 'ui/page';
+import { View } from 'ui/core/view';
+import { Animation, AnimationDefinition } from "ui/animation";
+import { Color } from 'color';
+import { SwipeGestureEventData, SwipeDirection } from 'ui/gestures';
+import * as enums from 'ui/enums';
 
 @Component({
   selector: 'app-dishdetail',
@@ -25,6 +31,10 @@ export class DishdetailComponent implements OnInit {
   dish: Dish;
   comment: Comment;
   errMess: string;
+  showComments: boolean = false;
+  cardImage: View;
+  commentList: View;
+  cardLayout: View;
 
   constructor (
     private favoriteservice: FavoriteService,
@@ -34,6 +44,7 @@ export class DishdetailComponent implements OnInit {
     private routerExtensions: RouterExtensions,
     private modalService: ModalDialogService,
     private vcRef: ViewContainerRef,
+    private page: Page,
     @Inject('baseURL') private baseURL
   ) {  }
 
@@ -50,6 +61,96 @@ export class DishdetailComponent implements OnInit {
         this.avgstars = (total/this.numcomments).toFixed(2);
       },
       errmess => { this.dish = null; this.errMess = <any>errmess; });
+  }
+
+  onSwipe(args: SwipeGestureEventData) {
+    if (this.dish) {
+      this.cardImage = <View>this.page.getViewById<View>("cardImage");
+      this.cardLayout = <View>this.page.getViewById<View>("cardLayout");
+      this.commentList = <View>this.page.getViewById<View>("commentList");
+
+      if (args.direction === SwipeDirection.up && !this.showComments ) {
+        this.animateUp();
+      }
+      else if (args.direction === SwipeDirection.down && this.showComments ) {
+        this.showComments = false;
+        this.animateDown();
+      }
+    }
+
+  }
+
+  showAndHideComments() {
+      this.cardImage = <View>this.page.getViewById<View>("cardImage");
+      this.cardLayout = <View>this.page.getViewById<View>("cardLayout");
+      this.commentList = <View>this.page.getViewById<View>("commentList");
+
+      if (!this.showComments ) {
+        this.animateUp();
+      }
+      else if (this.showComments ) {
+        this.showComments = false;
+        this.animateDown();
+      }
+  }
+
+  animateUp() {
+    let definitions = new Array<AnimationDefinition>();
+    let a1: AnimationDefinition = {
+        target: this.cardImage,
+        scale: { x: 1, y: 0 },
+        translate: { x: 0, y: -200 },
+        opacity: 0,
+        duration: 500,
+        curve: enums.AnimationCurve.easeIn
+    };
+    definitions.push(a1);
+
+    let a2: AnimationDefinition = {
+        target: this.cardLayout,
+        backgroundColor: new Color("#ffc107"),
+        duration: 500,
+        curve: enums.AnimationCurve.easeIn
+    };
+    definitions.push(a2);
+
+    let animationSet = new Animation(definitions);
+
+    animationSet.play().then(() => {
+      this.showComments = true;
+    })
+    .catch((e) => {
+        console.log(e.message);
+    });
+  }
+
+  animateDown() {
+    let definitions = new Array<AnimationDefinition>();
+    let a1: AnimationDefinition = {
+        target: this.cardImage,
+        scale: { x: 1, y: 1 },
+        translate: { x: 0, y: 0 },
+        opacity: 1,
+        duration: 500,
+        curve: enums.AnimationCurve.easeIn
+    };
+    definitions.push(a1);
+
+    let a2: AnimationDefinition = {
+        target: this.cardLayout,
+        backgroundColor: new Color("#ffffff"),
+        duration: 500,
+        curve: enums.AnimationCurve.easeIn
+    };
+    definitions.push(a2);
+
+    let animationSet = new Animation(definitions);
+
+    animationSet.play().then(() => {
+    })
+    .catch((e) => {
+        console.log(e.message);
+    });
   }
 
   addToFavorites(): void {
