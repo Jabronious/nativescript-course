2952024e679a3be1674a8b52ea5783558dd107c1
Jarrod Merryman <jarrodmerryman@gmail.com>
2019-01-27 15:00:12 -0500

    adds the initial swipe gestures

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,6 +5,11 @@ import { Promotion } from '../shared/promotion';
 import { PromotionService } from '../services/promotion.service';
 import { Leader } from '../shared/leader';
 import { LeaderService } from '../services/leader.service';
+import { TNSFontIconService } from 'nativescript-ngx-fonticon';
+import { Page } from 'ui/page';
+import { View } from 'ui/core/view';
+import { SwipeGestureEventData, SwipeDirection } from 'ui/gestures';
+import * as enums from 'ui/enums';
 
 @Component({
   selector: 'app-home',
@@ -19,10 +24,20 @@ export class HomeComponent implements OnInit {
   dishErrMess: string;
   promoErrMess: string;
   leaderErrMess: string;
+  showLeftCard: boolean = true;
+  showMiddleCard: boolean = false;
+  showRightCard: boolean = false;
+  leftCard: View;
+  middleCard: View;
+  rightCard: View;
 
-  constructor(private dishservice: DishService,
+
+  constructor(
+    private dishservice: DishService,
     private promotionservice: PromotionService,
     private leaderservice: LeaderService,
+    private page: Page,
+    private fonticon: TNSFontIconService,
     @Inject('baseURL') private baseURL) { }
 
     ngOnInit() {
@@ -37,4 +52,160 @@ export class HomeComponent implements OnInit {
           errmess => this.leaderErrMess = <any>errmess);
     }
 
+    onSwipe(args: SwipeGestureEventData) {
+      if (args.direction === SwipeDirection.left) {
+        this.animateLeft();
+      }
+      else if (args.direction === SwipeDirection.right) {
+        this.animateRight();
+      }
+    }
+
+    animateLeft() {
+      if (this.dish && this.promotion && this.leader) {
+        this.leftCard = this.page.getViewById<View>('leftCard');
+        this.middleCard  = this.page.getViewById<View>('middleCard');
+        this.rightCard  = this.page.getViewById<View>('rightCard');
+
+        if( this.showLeftCard ) {
+          this.rightCard.animate({
+            translate: { x: 2000, y: 0 }
+          })
+          .then(() => {
+            this.leftCard.animate({
+              translate: { x: -2000, y: 0 },
+              duration: 500,
+              curve: enums.AnimationCurve.easeInOut
+            })
+            .then(() => {
+              this.showLeftCard = false;
+              this.showMiddleCard = true;
+              this.middleCard.animate({
+                translate: { x: 0, y: 0 },
+                duration: 500,
+                curve: enums.AnimationCurve.easeInOut
+              });
+            });
+          });
+        }
+        else if( this.showMiddleCard ) {
+          this.leftCard.animate({
+            translate: { x: 2000, y: 0 },
+            duration: 500
+          })
+          .then(() => {
+            this.middleCard.animate({
+              translate: { x: -2000, y: 0 },
+              duration: 500,
+              curve: enums.AnimationCurve.easeInOut
+            })
+            .then(() => {
+              this.showMiddleCard = false;
+              this.showRightCard = true;
+              this.rightCard.animate({
+                translate: { x: 0, y: 0 },
+                duration: 500,
+                curve: enums.AnimationCurve.easeInOut
+              });
+            });
+          });
+        }
+        else if( this.showRightCard ) {
+          this.middleCard.animate({
+            translate: { x: 2000, y: 0 },
+            duration: 500
+          })
+          .then(() => {
+            this.rightCard.animate({
+              translate: { x: -2000, y: 0 },
+              duration: 500,
+              curve: enums.AnimationCurve.easeInOut
+            })
+            .then(() => {
+              this.showRightCard = false;
+              this.showLeftCard = true;
+              this.leftCard.animate({
+                translate: { x: 0, y: 0 },
+                duration: 500
+              });
+            });
+          });
+        }
+      }
+    }
+
+    animateRight() {
+
+    if (this.dish && this.promotion && this.leader) {
+      this.leftCard = this.page.getViewById<View>('leftCard');
+      this.middleCard  = this.page.getViewById<View>('middleCard');
+      this.rightCard  = this.page.getViewById<View>('rightCard');
+
+      if( this.showLeftCard ) {
+        this.middleCard.animate({
+          translate: { x: -2000, y: 0 },
+          duration: 500
+        })
+        .then(() => {
+          this.leftCard.animate({
+            translate: { x: 2000, y: 0 },
+            duration: 500,
+            curve: enums.AnimationCurve.easeInOut
+          })
+          .then(() => {
+            this.showLeftCard = false;
+            this.showRightCard = true;
+            this.rightCard.animate({
+              translate: { x: 0, y: 0 },
+              duration: 500,
+              curve: enums.AnimationCurve.easeInOut
+            });
+          });
+        });
+      }
+      else if( this.showMiddleCard ) {
+        this.rightCard.animate({
+          translate: { x: -2000, y: 0 },
+          duration: 500
+        })
+        .then(() => {
+          this.middleCard.animate({
+            translate: { x: 2000, y: 0 },
+            duration: 500,
+            curve: enums.AnimationCurve.easeInOut
+          })
+          .then(() => {
+            this.showMiddleCard = false;
+            this.showLeftCard = true;
+            this.leftCard.animate({
+              translate: { x: 0, y: 0 },
+              duration: 500,
+              curve: enums.AnimationCurve.easeInOut
+            });
+          });
+        });
+      }
+      else if( this.showRightCard ) {
+        this.leftCard.animate({
+          translate: { x: -2000, y: 0 },
+          duration: 500
+        })
+        .then(() => {
+          this.rightCard.animate({
+            translate: { x: 2000, y: 0 },
+            duration: 500,
+            curve: enums.AnimationCurve.easeInOut
+          })
+          .then(() => {
+            this.showRightCard = false;
+            this.showMiddleCard = true;
+            this.middleCard.animate({
+              translate: { x: 0, y: 0 },
+              duration: 500
+            });
+          });
+        });
+      }
+    }
   }
+}
--- src/app/shared/baseurl.js
+++ src/app/shared/baseurl.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-//export const baseURL = "http:192.168.1.69:3000/";
-exports.baseURL = "http://10.20.62.54:3000/";
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYmFzZXVybC5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbImJhc2V1cmwudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFBQSxtREFBbUQ7QUFDdEMsUUFBQSxPQUFPLEdBQUcsMEJBQTBCLENBQUMiLCJzb3VyY2VzQ29udGVudCI6WyIvL2V4cG9ydCBjb25zdCBiYXNlVVJMID0gXCJodHRwOjE5Mi4xNjguMS42OTozMDAwL1wiO1xuZXhwb3J0IGNvbnN0IGJhc2VVUkwgPSBcImh0dHA6Ly8xMC4yMC42Mi41NDozMDAwL1wiO1xuIl19
+exports.baseURL = "http:192.168.1.69:3000/";
+//export const baseURL = "http://10.20.62.54:3000/";
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYmFzZXVybC5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbImJhc2V1cmwudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFBYSxRQUFBLE9BQU8sR0FBRyx5QkFBeUIsQ0FBQztBQUNqRCxvREFBb0QiLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgY29uc3QgYmFzZVVSTCA9IFwiaHR0cDoxOTIuMTY4LjEuNjk6MzAwMC9cIjtcbi8vZXhwb3J0IGNvbnN0IGJhc2VVUkwgPSBcImh0dHA6Ly8xMC4yMC42Mi41NDozMDAwL1wiO1xuIl19
